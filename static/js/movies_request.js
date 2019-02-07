9ed6c8ecf984ee009ce065334f12367821ca594c
Manuel <ManuelLopezMallorquin@gmail.com>
2019-02-07 23:43:05 +0100

    a

--- static/js/movies_request.js
+++ static/js/movies_request.js
@@ -9,6 +9,6 @@ $(document).ready(function(){
     TmdbRequestFilter('#movies_vote_count', "https://api.themoviedb.org/3/discover/movie", {"languaje":"es-ES", "sort_by":"vote_count.desc", "without_genres": "16", "include_null_first_air_dates":"false.desc", "page":"1"}, "Peliculas mejor Votadas","movie");
 
     // En Cines
-    TmdbRequestFilter('#movies_theatres', "https://api.themoviedb.org/3/movie/now_playing", {"language":"es-ES", "page":"1"}, "Peliculas En Cartelera de Cines");
+    TmdbRequestFilter('#movies_theatres', "https://api.themoviedb.org/3/movie/now_playing", {"language":"es-ES", "page":"1"}, "Peliculas En Cartelera de Cines", "movie");
 
 });
