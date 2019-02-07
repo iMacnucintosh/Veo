da398c4d54bb59ec71c4423b9c63535e14b24a5c
Manuel <ManuelLopezMallorquin@gmail.com>
2019-02-07 23:41:21 +0100

    Peliculas mejor votadas redigiran a movies

--- static/js/movies_request.js
+++ static/js/movies_request.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     TmdbRequestFilter('#movies_popularity', "https://api.themoviedb.org/3/discover/movie", {"languaje":"es-ES", "sort_by":"popularity.desc", "without_genres": "16", "include_null_first_air_dates":"false.desc", "page":"1"}, "Peliculas Populares","movie");
 
     // Más Votadas
-    TmdbRequestFilter('#movies_vote_count', "https://api.themoviedb.org/3/discover/movie", {"languaje":"es-ES", "sort_by":"vote_count.desc", "without_genres": "16", "include_null_first_air_dates":"false.desc", "page":"1"}, "Peliculas mejor Votadas","show");
+    TmdbRequestFilter('#movies_vote_count', "https://api.themoviedb.org/3/discover/movie", {"languaje":"es-ES", "sort_by":"vote_count.desc", "without_genres": "16", "include_null_first_air_dates":"false.desc", "page":"1"}, "Peliculas mejor Votadas","movie");
 
     // En Cines
     TmdbRequestFilter('#movies_theatres', "https://api.themoviedb.org/3/movie/now_playing", {"language":"es-ES", "page":"1"}, "Peliculas En Cartelera de Cines");
