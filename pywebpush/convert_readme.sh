4c25c42140d76982d99fa4fe0efe90c3bee63cba
Manuel <iMacnucintosh@icloud.com>
2020-01-27 15:29:18 +0100

    Añadida de vuelta la grafica a la pantalla de inicio.

--- pywebpush/convert_readme.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# You will need pandoc to be installed for this to work correctly, as well as the PyPI packages docutils and pygments
-
-set -e
-pandoc --from=markdown --to=rst --output README.rst README.md
-python setup.py check --restructuredtext --strict --metadata
