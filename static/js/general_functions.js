23b9f972b8ba6bab7b9bd2373b79c2099e6be965
Manuel <iMacnucintosh@icloud.com>
2020-07-14 14:46:35 +0200

    First Upload

--- static/js/general_functions.js
+++ static/js/general_functions.js
@@ -205,7 +205,7 @@ function selectList(elemento, type, id_list) {
     $('#btnAddToList').attr("onclick", "addToList(" + type + "," + id_list + ")");
 }
 
-// Muestra un popup con la sinopsis del capitulo siguiente por ver en la página principal de Veo
+// Muestra un popup con la sinopsis del capitulo siguiente por ver en la página principal de TheRules
 function infoEpisode(overview){
     alert(overview);
 }
